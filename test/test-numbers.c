6bddfe1527729ad3d629cbcc6b8d8eaef7c6a538
detomon <simon@monoxid.ch>
2022-10-27 15:11:48 +0200

    Update build config

--- test/test-numbers.c
+++ test/test-numbers.c
@@ -12,7 +12,7 @@ int main(int argc, char const* argv[])
 	// check if character is a number
 	if (info->flags & UT_FLAG_NUMBER) {
 		// prints "Integer value of 0x2166: 7"
-		printf("Integer value of 0x%04X: %zd\n", glyph, info->num);
+		printf("Integer value of 0x%04X: %lld\n", glyph, info->num);
 	}
 	else {
 		return RESULT_FAIL;
